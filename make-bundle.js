88e68d620cffa1ec0d077a10381c3eab4226dd89
Brian McKenna <bmckenna@atlassian.com>
2011-07-05 10:10:45 +1000

    Use path.join

--- make-bundle.js
+++ make-bundle.js
@@ -6,7 +6,7 @@ var files = fs.readdirSync(dir);
 
 var output = 'var roy;(function(){var module={parent:true};var modules={};var load={};var require=function(x){if(!modules[x]){load[x](modules[x] = {})};return modules[x]}\n';
 files.forEach(function(file) {
-    var input = fs.readFileSync(dir + '/' + file, 'utf8');
+    var input = fs.readFileSync(path.join(dir, file), 'utf8');
     var name = JSON.stringify(path.basename(file, '.js'));
     output += 'load[' + name + '] = function(exports){' + input + '}\n';
 });
